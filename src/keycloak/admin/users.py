ed72704df3df5ee5d86984ab9c9ac1c2d3aff5f0
kashewnuts <kashun.nuts@gmail.com>
2021-10-11 09:49:44 +0900

    enable update username

--- src/keycloak/admin/users.py
+++ src/keycloak/admin/users.py
@@ -3,26 +3,27 @@ from collections import OrderedDict
 
 from keycloak.admin import KeycloakAdminBase
 
-__all__ = ('Users', 'User',)
+__all__ = (
+    "Users",
+    "User",
+)
 
 USER_KWARGS = [
-    'email',
-    'first_name',
-    'last_name',
-    'email_verified',
-    'attributes',
-    'realm_roles',
-    'client_roles',
-    'groups',
-    'enabled',
-    'credentials'
+    "email",
+    "first_name",
+    "last_name",
+    "email_verified",
+    "attributes",
+    "realm_roles",
+    "client_roles",
+    "groups",
+    "enabled",
+    "credentials",
 ]
 
 
 class Users(KeycloakAdminBase):
-    _paths = {
-        'collection': '/auth/admin/realms/{realm}/users'
-    }
+    _paths = {"collection": "/auth/admin/realms/{realm}/users"}
 
     _realm_name = None
 
@@ -47,14 +48,15 @@ class Users(KeycloakAdminBase):
 
         for key in USER_KWARGS:
             from keycloak.admin.clientroles import to_camel_case
+
             if key in kwargs:
                 payload[to_camel_case(key)] = kwargs[key]
 
         return self._client.post(
             url=self._client.get_full_url(
-                self.get_path('collection', realm=self._realm_name)
+                self.get_path("collection", realm=self._realm_name)
             ),
-            data=json.dumps(payload, sort_keys=True)
+            data=json.dumps(payload, sort_keys=True),
         )
 
     def all(self):
@@ -65,21 +67,17 @@ class Users(KeycloakAdminBase):
         """
         return self._client.get(
             url=self._client.get_full_url(
-                self.get_path('collection', realm=self._realm_name)
+                self.get_path("collection", realm=self._realm_name)
             )
         )
 
     def by_id(self, user_id):
-        return User(realm_name=self._realm_name,
-                    user_id=user_id, client=self._client)
+        return User(realm_name=self._realm_name, user_id=user_id, client=self._client)
 
 
 class User(KeycloakAdminBase):
     _BASE = "/auth/admin/realms/{realm}/users/{user_id}"
-    _paths = {
-        'single': _BASE,
-        'reset_password': _BASE + "/reset-password"
-    }
+    _paths = {"single": _BASE, "reset_password": _BASE + "/reset-password"}
 
     def __init__(self, realm_name, user_id, *args, **kwargs):
         self._realm_name = realm_name
@@ -96,16 +94,18 @@ class User(KeycloakAdminBase):
     @property
     def role_mappings(self):
         from keycloak.admin.user.userroles import UserRoleMappings
-        return UserRoleMappings(realm_name=self._realm_name,
-                                user_id=self._user_id,
-                                client=self._client)
+
+        return UserRoleMappings(
+            realm_name=self._realm_name, user_id=self._user_id, client=self._client
+        )
 
     @property
     def groups(self):
         from keycloak.admin.user.usergroup import UserGroups
-        return UserGroups(realm_name=self._realm_name,
-                          user_id=self._user_id,
-                          client=self._client)
+
+        return UserGroups(
+            realm_name=self._realm_name, user_id=self._user_id, client=self._client
+        )
 
     def get(self):
         """
@@ -115,9 +115,7 @@ class User(KeycloakAdminBase):
         """
         self._user = self._client.get(
             url=self._client.get_full_url(
-                self.get_path(
-                    'single', realm=self._realm_name, user_id=self._user_id
-                )
+                self.get_path("single", realm=self._realm_name, user_id=self._user_id)
             )
         )
         self._user_id = self.user["id"]
@@ -129,6 +127,7 @@ class User(KeycloakAdminBase):
 
         https://www.keycloak.org/docs-api/2.5/rest-api/index.html#_userrepresentation
 
+        :param str username: username for user
         :param str first_name: first_name for user
         :param str last_name: last_name for user
         :param str email: Email for user
@@ -141,17 +140,16 @@ class User(KeycloakAdminBase):
         payload = {}
         for k, v in self.user.items():
             payload[k] = v
-        for key in USER_KWARGS:
+        for key in USER_KWARGS + ["username"]:
             from keycloak.admin.clientroles import to_camel_case
+
             if key in kwargs:
                 payload[to_camel_case(key)] = kwargs[key]
         result = self._client.put(
             url=self._client.get_full_url(
-                self.get_path(
-                    'single', realm=self._realm_name, user_id=self._user_id
-                )
+                self.get_path("single", realm=self._realm_name, user_id=self._user_id)
             ),
-            data=json.dumps(payload, sort_keys=True)
+            data=json.dumps(payload, sort_keys=True),
         )
         self.get()
         return result
@@ -162,25 +160,18 @@ class User(KeycloakAdminBase):
         """
         self._user = self._client.delete(
             url=self._client.get_full_url(
-                self.get_path(
-                    'single', realm=self._realm_name, user_id=self._user_id
-                )
+                self.get_path("single", realm=self._realm_name, user_id=self._user_id)
             )
         )
 
     def reset_password(self, password, temporary=False):
-        payload = {
-            "type": "password",
-            "value": password,
-            "temporary": temporary
-        }
+        payload = {"type": "password", "value": password, "temporary": temporary}
         result = self._client.put(
             url=self._client.get_full_url(
                 self.get_path(
-                    'reset_password', realm=self._realm_name,
-                    user_id=self._user_id
+                    "reset_password", realm=self._realm_name, user_id=self._user_id
                 )
             ),
-            data=json.dumps(payload, sort_keys=True)
+            data=json.dumps(payload, sort_keys=True),
         )
         return result
